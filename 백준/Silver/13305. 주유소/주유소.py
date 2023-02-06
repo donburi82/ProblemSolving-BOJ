31114b76191d53560b3f53197cf8d0afbe0918d5
donburi82 <104419950+donburi82@users.noreply.github.com>
2023-02-06 15:37:00 +0800

    Update 주유소.py

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,17 +1,11 @@
 import sys
 
 n = int(sys.stdin.readline())
-dist = list(map(int, sys.stdin.readline().split()))
-stations = list(map(int, sys.stdin.readline().split()))
-total = 0
-i = 0
-while i < len(stations)-1:
-    if stations[i] <= stations[i+1]:
-        travel = dist[i]
-        travel += dist[i+1] if i+1<len(stations)-1 else 0
-        total += stations[i]*travel
-        i += 2
-    else:
-        total += stations[i]*dist[i]
-        i += 1
+dists = list(map(int, sys.stdin.readline().split()))
+costs = list(map(int, sys.stdin.readline().split()))
+total = dists[0]*costs[0]
+min_cost = costs[0]
+for i in range(1, len(dists)):
+    min_cost = min(min_cost, costs[i])
+    total += min_cost*dists[i]
 print(total)
