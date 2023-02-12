cea2dddcf7355d7d5c4095834de3f83ed57dd1e7
donburi82 <104419950+donburi82@users.noreply.github.com>
2023-02-12 17:26:48 +0800

    [Silver II] Title: 신나는 함수 실행, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,21 +1,21 @@
 import sys
-
-mem = dict()
+dp = [[[0]*21 for _ in range(21)] for _ in range(21)]
 
 def w(a, b, c):
-    if (a, b, c) in mem:
-        return mem[(a, b, c)]
     if a<=0 or b<=0 or c<=0:
-        mem[(a, b, c)] = 1
-        return mem[(a, b, c)]
+        return 1
     if a>20 or b>20 or c>20:
-        mem[(a, b, c)] = w(20, 20, 20)
-        return mem[(a, b, c)]
+        return w(20, 20, 20)
+    
+    if dp[a][b][c]:
+        return dp[a][b][c]
+
     if a<b and b<c:
-        mem[(a, b, c)] = w(a, b, c-1)+w(a, b-1, c-1)-w(a, b-1, c)
-        return mem[(a, b, c)]
-    mem[(a, b, c)] = w(a-1, b, c)+w(a-1, b-1, c)+w(a-1, b, c-1)-w(a-1, b-1, c-1)
-    return mem[(a, b, c)]
+        dp[a][b][c] = w(a, b, c-1)+w(a, b-1, c-1)-w(a, b-1, c)
+        return dp[a][b][c]
+    
+    dp[a][b][c] = w(a-1, b, c)+w(a-1, b-1, c)+w(a-1, b, c-1)-w(a-1, b-1, c-1)
+    return dp[a][b][c]
 
 while True:
     a, b, c = map(int, sys.stdin.readline().split())
